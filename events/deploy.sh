886d5e360e6982a039eee9019aabc25a74339518
Patricia <pthom2503@gmail.com>
2021-03-06 02:23:25 +0000

    deployment work

---
+++ events/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+# Common port range for this is 4000-10,000
+# Valid port range for a user app to listen
+# on is something like 1025-32767
+export PORT=4803
+export SECRET_KEY_BASE=insecure
+
+mix deps.get --only prod
+mix compile
+mix ecto.create
+mix ecto.migrate
+
+CFGD=$(readlink -f ~/.config/events)
+
+if [ ! -d "$CFGD" ]; then
+    mkdir -p "$CFGD"
+fi
+
+if [ ! -e "$CFGD/base" ]; then
+    mix phx.gen.secret > "$CFGD/base"
+fi
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+npm install --prefix ./assets
+npm run deploy --prefix ./assets
+mix phx.digest
+
+mix release
---
+++ events/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4803
+
+CFGD=$(readlink -f ~/.config/events)
+
+if [ ! -e "$CFGD/base" ]; then
+    echo "run deploy first"
+    exit 1
+fi
+
+DATABASE_URL=""
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+_build/prod/rel/events/bin/events start
